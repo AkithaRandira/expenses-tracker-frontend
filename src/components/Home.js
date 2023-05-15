ffdf549cc229ab79c05f004dc9f2a28f229cec11
AkithaRandira <113655602+AkithaRandira@users.noreply.github.com>
2023-05-15 21:11:56 +0530

    Adding front end

---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import "./HomeStyles.css";
+
+
+
+function Home() {
+  return (
+    <>
+
+
+      <div className="home">
+        
+        <div className="section-container">
+            <div className="columns image">
+
+               &nbsp;
+            </div>
+            <div className="columns content">
+               <div className="content-container">
+                  <h2>Saverly</h2>
+                  <p>
+                  Welcome to Saverly, the ultimate expenses tracking website that helps you take control of your finances like never before! With Saverly, you can easily manage your expenses, keep track of your spending habits, and make smarter financial decisions. Our user-friendly interface and intuitive features make it easy for anyone to use, regardless of their financial expertise.
+                  </p>
+                  <p>
+                  Say goodbye to the hassle of manual expense tracking and hello to Saverly's automated expense tracking system. Our website provides you with a simple and efficient way to keep track of your expenses and monitor your budget. Plus, with our detailed reports and charts, you can easily analyze your spending habits and identify areas where you can save money.
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
