106fb00969f2010ce5f8f306bc6a2a6f54395a11
Rasa-20 <rasandikarunarathna@gmail.com>
2023-05-21 14:38:09 +0530

    Adding add expense page and add income page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import "./styles.css";
 import Footer from "./components/Footer";
+import AddIncome from "./components/AddIncome";
+import AddExpense from "./components/AddExpense";
+
 
 export default function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
       <Navbar />
-      <Home />
+      <Route path="/" exact component={Home}/>
+      <Route path="/addincome" exact component={AddIncome}/>
+      <Route path="/addexpense" exact component={AddExpense}/>
       <Footer/>
-    </div>
+      </div>
+    </Router>
   );
 }
