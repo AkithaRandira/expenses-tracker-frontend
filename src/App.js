7fdce216cec16cb69d64cd14077ad34e8c037f2c
Sumuditha Lansakara <113225997+LaXnZ@users.noreply.github.com>
2023-05-21 15:37:27 +0530

    Updating routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import {BrowserRouter as Router,Route} from "react-router-dom";
 import "./styles.css";
 import Footer from "./components/Footer";
+import UpdateUser from "./components/UpdateUser";
+import DashBoard from "./components/DashBoard";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 import AddIncome from "./components/AddIncome";
 import AddExpense from "./components/AddExpense";
-
 
 export default function App() {
   return (
@@ -13,8 +13,12 @@ export default function App() {
       <div>
       <Navbar />
       <Route path="/" exact component={Home}/>
-      <Route path="/addincome" exact component={AddIncome}/>
-      <Route path="/addexpense" exact component={AddExpense}/>
+            <Route path="/UpdateUser" exact component={UpdateUser}/> 
+            <Route path="/DashBoard" exact component={DashBoard}/>
+            <Route path="/signup" exact component= {SignUp} />
+            <Route path="/signin" exact component= {SignIn} />
+            <Route path="/addincome" exact component={AddIncome}/>
+            <Route path="/addexpense" exact component={AddExpense}/>
       <Footer/>
       </div>
     </Router>
