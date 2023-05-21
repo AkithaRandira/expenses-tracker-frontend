2496c7ea321ce87367a80a69a38587ef8be7b37b
2002yo <yohansa1217@gmail.com>
2023-05-21 14:55:26 +0530

    Adding Update user profile and Dashboard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import {BrowserRouter as Router,Route} from "react-router-dom"
 
 import "./styles.css";
 import Footer from "./components/Footer";
+import UpdateUser from "./components/UpdateUser";
+import DashBoard from "./components/DashBoard";
 
 export default function App() {
   return (
-    <div className="App">
+    <Router>
+    <div>
       <Navbar />
-      <Home />
+            <Route path="/" exact component={Home}/>
+            <Route path="/UpdateUser" exact component={UpdateUser}/> 
+            <Route path="/DashBoard" exact component={DashBoard}/>
       <Footer/>
     </div>
-  );
+    </Router>
+  ); 
 }
