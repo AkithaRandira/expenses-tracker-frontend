6915f3fcadbe12be6ac9461a97e2b1fbb4fa27aa
LaXnZ <sumudithalanz@gmail.com>
2023-05-22 21:05:29 +0530

    Login user action and reducer

--- src/redux/slices/users/usersSlices.js
+++ src/redux/slices/users/usersSlices.js
@@ -1,7 +1,7 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-//Login action
+//login action
 export const loginUserAction = createAsyncThunk(
   "user/login",
   async (payload, { rejectWithValue, getState, dispatch }) => {
@@ -24,3 +24,34 @@ export const loginUserAction = createAsyncThunk(
     }
   }
 );
+
+//slices
+const userSlices = createSlice({
+  name: "users",
+  initialState: {},
+  extraReducers: (builder) => {
+    //handle pending state
+    builder.addCase(loginUserAction.pending, (state, action) => {
+      state.userLoading = true;
+      state.userAppError = undefined;
+      state.userServerError = undefined;
+    });
+
+    //handle success state
+    builder.addCase(loginUserAction.fulfilled, (state, action) => {
+      state.userAuth = action?.payload;
+      state.userLoading = false;
+      state.userAppError = undefined;
+      state.userServerError = undefined;
+    });
+
+    //handle rejected state
+    builder.addCase(loginUserAction.rejected, (state, action) => {
+      state.userLoading = false;
+      state.userAppError = action?.payload?.message;
+      state.userServerError = action?.error?.message;
+    });
+  },
+});
+
+export default userSlices.reducer;
