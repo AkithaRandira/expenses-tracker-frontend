9766fbb43effe20fbb7fa8fe0c4714365f9c231d
LaXnZ <sumudithalanz@gmail.com>
2023-06-01 23:20:01 +0530

    Display data using a pie chart

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import ErrorDisplayMessage from "../components/ErrorDisplayMessage";
 import Loading from "../components/Loading";
 import { fetchAccountStatsAction } from "../redux/slices/accountStatistics/accountStatSlices";
+import { GraphData } from "./GraphData";
 
 export default function UpdateUser() {
   const dispatch = useDispatch();
@@ -19,39 +20,46 @@ export default function UpdateUser() {
     <div className="Content">
       <NavbarAfterLogin />
       <button class="updatebtn">Edit profile</button>
+      <GraphData
+        income={accountDetails?.incomeStats[0]?.totalIncome}
+        expense={accountDetails?.expenseStats[0]?.totalExpense}
+      />
       <div className="dashboard">
         <div className="DashboardItems1">
           <span className="mainTitle">EXPENSES</span>
           <div className="Container">
             <br></br>
             <span className="title">Total Expense</span>
-            <h2>{accountDetails?.expenseStats[0].totalExpense}</h2>
+            <h2>{accountDetails?.expenseStats[0]?.totalExpense}</h2>
             <br></br> <span className="title">Number of Transactions</span>
-            <h2>{accountDetails?.expenseStats[0].totalRecordsExpense}</h2>
+            <h2>{accountDetails?.expenseStats[0]?.totalRecordsExpense}</h2>
             <br></br> <span className="title">Minimum Transaction</span>
-            <h2>{accountDetails?.expenseStats[0].minExpense}</h2>
+            <h2>{accountDetails?.expenseStats[0]?.minExpense}</h2>
             <br></br> <span className="title">Maximum Transaction</span>
-            <h2>{accountDetails?.expenseStats[0].maxExpense}</h2>
+            <h2>{accountDetails?.expenseStats[0]?.maxExpense}</h2>
             <br></br> <span className="title">Average</span>
-            <h2>{accountDetails?.expenseStats[0].averageExpense}</h2>
+            <h2>{accountDetails?.expenseStats[0]?.averageExpense}</h2>
           </div>
           <br></br>
-          <a type="button" class="button01" href="/expense-list"> View Expenses history </a>
+          <a type="button" class="button01" href="/expense-list">
+            {" "}
+            View Expenses history{" "}
+          </a>
         </div>
         <div className="DashboardItems2">
           <span className="mainTitle">INCOME</span>
           <div className="Container">
             <br></br>
             <span className="title">Total Income</span>
-            <h2>{accountDetails?.incomeStats[0].totalIncome}</h2>
+            <h2>{accountDetails?.incomeStats[0]?.totalIncome}</h2>
             <br></br> <span className="title">Number of Transactions</span>
-            <h2>{accountDetails?.incomeStats[0].totalRecordsIncome}</h2>
+            <h2>{accountDetails?.incomeStats[0]?.totalRecordsIncome}</h2>
             <br></br> <span className="title">Minimum Transaction</span>
-            <h2>{accountDetails?.incomeStats[0].minIncome}</h2>
+            <h2>{accountDetails?.incomeStats[0]?.minIncome}</h2>
             <br></br> <span className="title">Maximum Transaction</span>
-            <h2>{accountDetails?.incomeStats[0].maxIncome}</h2>
+            <h2>{accountDetails?.incomeStats[0]?.maxIncome}</h2>
             <br></br> <span className="title">Average</span>
-            <h2>{accountDetails?.incomeStats[0].averageIncome}</h2>
+            <h2>{accountDetails?.incomeStats[0]?.averageIncome}</h2>
           </div>
           <br></br>
           <a type="button" class="button02" href="/income-list">
